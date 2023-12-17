c71ac822f8187ec440cecf83fc3cc1f193df9d35
MilutinTokic <153679507+MilutinTokic@users.noreply.github.com>
2023-12-17 17:22:49 +0100

    input_reader.py

--- input_reader.py
+++ input_reader.py
@@ -1,5 +1,13 @@
+import math
+
 def read_input(file_path):
     with open(file_path, 'r') as file:
         data = file.readlines()
         # Ovdje dodajte kod za obradu ulaznih podataka
     return processed_data
+
+def calculate_distance(point1, point2):
+   x1, y1 = point1
+   x2, y2 = point2
+   distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+   return distance
