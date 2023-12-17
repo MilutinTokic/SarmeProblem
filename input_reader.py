ac0ac54001f7e27faee4d2282b04ff19e2b554fc
MilutinTokic <153679507+MilutinTokic@users.noreply.github.com>
2023-12-17 17:15:35 +0100

    Create input_reader.py

---
+++ input_reader.py
@@ -0,0 +1,5 @@
+def read_input(file_path):
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+        # Ovdje dodajte kod za obradu ulaznih podataka
+    return processed_data
